b45b70038737896912733c5023fbc3f31a1eed38
mcwplimaUcs <mcwplima@ucs.br>
2019-04-09 22:21:23 -0300

    Release 1.0

--- consultor_financeiro.pl
+++ consultor_financeiro.pl
@@ -1,9 +1,9 @@
 %cliente(nome,renda_mensal,renda_estavel,poupanca,dependentes)
-cliente(anderson,1917,estavel,5700,2).
+cliente(anderson,1900,estavel,5700,2).
 cliente(andressa,2100,estavel,9740,1).
 cliente(bruna,4670,instavel,1450,1).
 cliente(camila,3780,estavel,12300,2).
-cliente(caroline,5800,instavel,23000,1).
+cliente(caroline,2000,instavel,23000,1).
 cliente(daiane,4100,estavel,32000,3).
 cliente(eduardo,980,instavel,2600,1).
 cliente(guilherme,3320,instavel,10680,2).
@@ -14,47 +14,66 @@ cliente(marina,3100,instavel,7000,2).
 cliente(otavio,4852,instavel,5000,3).
 cliente(rochele,5900,estavel,23200,1).
 cliente(samara,940,estavel,4850,2).
+cliente(jeferson,0,estavel,0,2).
 
 %---------------------------------------------------------------------------------
 
-%verifica_poupanca_minima(Cliente,Adequacao).
-verifica_poupanca_minima(Nome_cliente,adequada):-
-    cliente(Nome_cliente,_,_,Poupanca,Num_dependentes),
-    Valor is Num_dependentes*5000,
-    Poupanca>=Valor,!.
 
-verifica_poupanca_minima(Nome_cliente,inadequada):-
-    cliente(Nome_cliente,_,_,Poupanca,Num_dependentes),
-    Valor_necessario is Num_dependentes*5000-Poupanca,
-    write("O valor em poupança para o cliente "), write(Nome_cliente), write(" esta abaixo do adequado! Necessita de mais R$"), write(Valor_necessario).
+%maior_poupanca
+maior_poupanca(Cliente,true):-
+    cliente(Cliente,_,_,Poupanca,Nro_dep),
+    Poupanca > 0,
+    Nro_dep > 0,
+    Valor is Nro_dep*5000 + 5000,
+    Poupanca>=Valor, !. 
+maior_poupanca(Cliente,true):-
+    cliente(Cliente,_,_,Poupanca,Nro_dep),
+    Poupanca > 0,
+    Nro_dep == 0,
+    Poupanca>=5000, !.
+maior_poupanca(_,false).
 
-%verifica_renda(Cliente)
-verifica_renda(Nome_cliente):-
-    cliente(Nome_cliente,Renda_mensal,estavel,_,Num_dependentes),
+%verifica_poupanca_minima
+verifica_poupanca_minima(Cliente,adequada):-
+    maior_poupanca(Cliente,Res),
+    Res == true,!.
+verifica_poupanca_minima(_,inadequada).
+
+%verifica_renda_minima
+verifica_renda_minima(Cliente,adequada):-
+    cliente(Cliente,Renda_mensal,Estado,_,Num_dependentes),
+    Estado == estavel,
     Renda_anual is Renda_mensal*12,
+    Num_dependentes > 0,
     Necessario_dependente is Num_dependentes*4000,
     Renda_anual_necessaria is 15000 + Necessario_dependente,
     Renda_anual>=Renda_anual_necessaria,!.
 
-verifica_renda(Nome_cliente):-
-    cliente(Nome_cliente,Renda_mensal,_,_,Num_dependentes),
+verifica_renda_minima(Cliente,adequada):-
+    cliente(Cliente,Renda_mensal,Estado,_,Num_dependentes),
+    Estado == estavel,
     Renda_anual is Renda_mensal*12,
-    Necessario_dependente is Num_dependentes*4000,
-    Renda_anual_necessaria is 15000 + Necessario_dependente,
-    write("\nA renda anual é inadequada! Necessita de R$"), write(Renda_anual_necessaria), write(" reais anuais"), write(". Possui atualmente R$"), write(Renda_anual), write("/ano").
+    Num_dependentes == 0,
+    Renda_anual>=15000,!.
+verifica_renda_minima(_,inadequada).
 
-%verifica_aplicacoes(Cliente, Resposta)
-verifica_aplicacoes(Cliente, Resposta):-
-    verifica_poupanca_minima(Cliente, Resposta),
-    verifica_renda(Cliente),
-    write("\nO cliente "), write(Cliente), write(" possui uma conta poupança e renda adequadas! Sugiro um investimento mais arriscado, como o mercado de ações!").
+consultor(Cliente):-
+    verifica_poupanca_minima(Cliente,inadequada),
+    write("\n Prezado cliente, você não tem quantia poupada. \n Junte esforços para aumentar essa quantia!!"),!.
+consultor(Cliente):-
+    verifica_poupanca_minima(Cliente,adequada),
+    verifica_renda_minima(Cliente,adequada),
+    write("\n Prezado cliente, você já se esforçou bastante. \n Está na hora de evoluir, invista em ações!!"),!.
+consultor(Cliente):-
+    verifica_poupanca_minima(Cliente,adequada),
+    verifica_renda_minima(Cliente,inadequada),
+    write("\n Prezado cliente, você percorreu um belo caminho, mas ainda há muito o que fazer. \n Divida os seus investimentos entre a poupança e ações!!"),!.
+consultor(Cliente):-
+    cliente(Cliente,_,_,_,_),
+    write("\n"), write(Cliente), write("Você ainda não é um investidor. \n Está na hora de começar!!"),!.
 
-
-
-
-
-
-
-
-% repetiçoes de variaveis nos predicados?
-% verifica_aplicacoes(samara,Resposta).
+principal(Cliente):-
+    cliente(Cliente,_,_,_,_),
+    consultor(Cliente),!.
+principal(_):-
+    write("Cliente desconhecido").
